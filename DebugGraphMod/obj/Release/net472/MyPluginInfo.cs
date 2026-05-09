0baabb41cd8b95bff0e8851dba2dde969d48395e
AlEX-FRiT <alexfrit@gmail.com>
2026-05-10 01:33:33 +0800

    Add .gitignore for DebugGraphMod, untrack bin/obj

--- DebugGraphMod/obj/Release/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace DebugGraphMod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "DebugGraphMod";
-        public const string PLUGIN_NAME = "DebugGraphMod";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
